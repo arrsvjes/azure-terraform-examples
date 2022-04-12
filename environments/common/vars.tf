762fd225570a9456a1f2365d15064bcbd433a7be
arrsvjes <88336300+arrsvjes@users.noreply.github.com>
2022-04-12 15:21:13 -0500

    Update vars.tf

--- environments/common/vars.tf
+++ environments/common/vars.tf
@@ -1,13 +1,13 @@
 variable "cidr_block" { default = "10.0.0.0/16" }
 variable "compute_disk_size" { default = "30" }
-variable "compute_instance_size" { default = "Standard_B1ms" }
+variable "compute_instance_size" { default = "D2as v4" }
 variable "compute_username" { default = "cloud-user" }
 variable "database_name" { default = "testdb" }
 variable "database_sku_name" { default = "" }
 variable "database_username" { default = "psqladmin" }
 variable "database_password" { default = "C44Tr@in1ng" }
 variable "env" { }
-variable "location" { default = "East US 2" }
+variable "location" { default = "North Europe" }
 variable "name" { }
 variable "ssh_key_file" { default = "~/.ssh/id_rsa.pub" }
 variable "use_compute_module" { default = true }
